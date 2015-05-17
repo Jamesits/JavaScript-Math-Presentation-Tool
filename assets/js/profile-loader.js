5d273f0a544b85e766716d26a81e3e53a6794225
James Swineson <jamesswineson@gmail.com>
2015-05-18 00:05:49 +0800

    Navbar working correctly

--- assets/js/profile-loader.js
+++ assets/js/profile-loader.js
@@ -7,9 +7,37 @@ var config = {
 		{
 			"id": "iteration",
 			"name": "Iteration"
+		},
+		{
+			"id": "newton",
+			"name": "Newton"
 		}
 	]
 };
 
+// use hash to get current profile
+
+function set_current_profile(id) {
+	for (profile in config.profiles) {
+		if (profile.id == id) {
+			window.location.hash = id;
+			break;
+		}
+	}
+	if ($.isNumeric(id) && id < config.profiles.length) {
+		window.location.hash = config.profiles[id].id;
+	}
+	$("#navbar-method li").removeClass("active");
+	$(window.location.hash).addClass("active");
+}
+
+function get_current_profile() {
+	if (window.location.hash.slice(1) == "")
+		set_current_profile(0);
+	return window.location.hash.slice(1);
+}
+
+// Fill nav bar
 navbar_templates = $("#template-navbar-method").html();
 $("#navbar-method").html(Mustache.render(navbar_templates, config));
+current_profile = get_current_profile();
